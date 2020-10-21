11e4992cc39fa8d27a6d1647e1a70a875e26eec8
OneFirewall User <join@onefirewall.com>
2020-10-22 00:32:09 +0100

    Add install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+###########################################
+#        OneFirewall Alliance LTD         #
+###########################################
+
+wget https://github.com/onefirewall/WCF-Agent/archive/latet.zip
+unzip latet.zip
+cd WCF-Agent-latet
+npm install
