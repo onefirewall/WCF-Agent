fdba9fe7ebf4e6199911abaaadcdb9a5a7f1708c
Sotiraki Sima <sotiraq.sima@gmail.com>
2021-04-24 12:08:46 +0100

    Checkpoint Artifacts

---
+++ install-direct.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+###########################################
+#        OneFirewall Alliance LTD         #
+#                  2021                   #
+###########################################
+echo "WV1"
+
+# pre-install
+rm -rf WCF-Agent-latest/
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs zip
+
+wget https://github.com/onefirewall/WCF-Agent/archive/latest.zip
+unzip latest.zip
+rm -rf latest.zip
+cd WCF-Agent-latest
+
+# Remove not neccessary files
+npm install
+node app test
+rm -rf .github/
+rm -rf images/
+rm -rf test/
+rm -rf .gitignore
+rm -rf README.md
+rm -rf install.sh
+cd ..
+
+# remove previous installation
+sudo rm -rf /opt/onefirewall/WCF-Agent-latest
+
+pwd
+ls
+# copy folder
+sudo mkdir -p /opt/onefirewall/
+sudo cp -r WCF-Agent-latest /opt/onefirewall/
+
+#(crontab -l ; echo "* * * * * cd /opt/onefirewall/WCF-Agent-latest && bash run.sh")| crontab -
--- run.sh
+++ run.sh
@@ -3,30 +3,31 @@
 #        OneFirewall Alliance LTD         #
 ###########################################
 
+cd /opt/onefirewall/WCF-Agent-latest
 INSTALLED_DIR=$(pwd)
 CONFIG_JSON=config.json
 LOCKFILE=lock.txt
 EXEC_FILE=onefirewall-agent
 
-cd $INSTALLED_DIR;
+#cd $INSTALLED_DIR;
+
+H=$(date +%H) # Hour
+M=$(date +%M) # Minute
+d=$(date +%d) # Day
+
+# Reset logs
+if (( (1 == 10#$d || 10#$d == 5 || 10#$d == 7 || 10#$d == 10 || 10#$d == 15 || 10#$d == 20 || 10#$d == 25) && (1 == 10#$H) && (0 == 10#$M) )); then 
+    rm -rf stderr.log stdout.log;
+fi
 
 if [ ! -f $LOCKFILE ]; then
     touch $LOCKFILE
     echo "$LOCKFILE created" >> stdout.log;
 
-    H=$(date +%H) # hour
-    M=$(date +%M) # Minute
-    d=$(date +%d) # Day
-
-    # Reset logs on 1:00am every 1 and 15 of the months
-    if (( (1 == 10#$d || 10#$d == 5 || 10#$d == 7 || 10#$d == 10 || 10#$d == 15 || 10#$d == 20 || 10#$d == 25) && (1 == 10#$H) && (0 == 10#$M) )); then 
-        rm -rf stderr.log stdout.log;
-    fi
-
-    echo ".........::::::::::::::: START IDS :::::::::::::::........." >> stdout.log;
+    echo ".........::::::::::::::: START OneFirewall IDS :::::::::::::::........." >> stdout.log;
     node app ids $INSTALLED_DIR/$CONFIG_JSON >>stderr.log 2>&1 >> stdout.log;
 
-    echo ".........::::::::::::::: START IPS :::::::::::::::........." >> stdout.log;
+    echo ".........::::::::::::::: START OneFirewall IPS :::::::::::::::........." >> stdout.log;
     node app ips $INSTALLED_DIR/$CONFIG_JSON >>stderr.log 2>&1 >> stdout.log;
 
     rm -rf $LOCKFILE
